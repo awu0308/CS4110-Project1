fffb71d9fd23b5748d6f7748771cd670ae7f058c
awu0308 <alexwu0308@gmail.com>
2024-11-11 18:18:10 -0500

    added random_utils.py docstring

--- docstrings_testing/meal_max/meal_max/utils/random_utils.py
+++ docstrings_testing/meal_max/meal_max/utils/random_utils.py
@@ -8,6 +8,16 @@ configure_logger(logger)
 
 
 def get_random() -> float:
+    """
+    Fetches a random 2 decimal float between 0 and 1 from random.org
+
+    Returns:
+        float: the random number from random.org.
+
+    Raises:
+        RuntimeError: if the request to random.org fails or returns an invalid response
+        ValueError: if the response from random.org is not a valid float
+    """
     url = "https://www.random.org/decimal-fractions/?num=1&dec=2&col=1&format=plain&rnd=new"
 
     try:
